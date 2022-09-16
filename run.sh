022c41733ca708e5e55433a37946559d56b989ea
felix0496 <felix.kirsch@kern.ai>
2022-09-16 11:07:17 +0200

    implements running of attribute calculation

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-/usr/local/bin/python run_ac.py;
+/usr/bin/curl -s "$1" > docbin_full.json;
+/usr/bin/curl -s "$2" > attribute_calculators.py;
+
+/usr/local/bin/python run_ac.py "$3" "$4";
