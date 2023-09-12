86641dd0b6f1200015abe586f897ad305e3e1c76
JWittmeyer <jens.wittmeyer@kern.ai>
2023-09-12 15:10:54 +0200

    Adds knowledge to ac exec env

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 /usr/bin/curl -s "$1" > docbin_full.json;
 /usr/bin/curl -s "$2" > attribute_calculators.py;
+/usr/bin/curl -s "$3" > knowledge.py;
 
-/usr/local/bin/python -u run_ac.py "$3" "$4" "$5";
+/usr/local/bin/python -u run_ac.py "$4" "$5" "$6";
