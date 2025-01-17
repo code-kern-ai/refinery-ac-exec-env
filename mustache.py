23ecd8a96a74633739f79b481bf76935786a4b91
andhreljaKern <andrea.hrelja@kern.ai>
2025-01-17 11:44:58 +0100

    perf: mustache align to requirements

--- mustache.py
+++ mustache.py
@@ -4,7 +4,7 @@ import chevron
 
 def prepare_and_render_mustache(template_text: str, record_dict: Dict[str, Any]) -> str:
     comb_dict = {
-        "rd": record_dict,
+        "record": record_dict["data"],
     }
     return chevron.render(
         template=template_text,
