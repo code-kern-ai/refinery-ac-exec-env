73ac7ac5acb1579996cb90514509234bc95c5c19
andhreljaKern <andrea.hrelja@kern.ai>
2025-01-23 14:03:45 +0100

    fix: mustache rendering

--- mustache.py
+++ mustache.py
@@ -3,10 +3,7 @@ import chevron
 
 
 def prepare_and_render_mustache(template_text: str, record_dict: Dict[str, Any]) -> str:
-    comb_dict = {
-        "record": record_dict["data"],
-    }
     return chevron.render(
         template=template_text,
-        data=comb_dict,
+        data=record_dict["data"],
     )
